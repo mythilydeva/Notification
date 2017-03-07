1a943e3bab4f412151800f40ded543f606e1a049
ITLAB <ITLAB@IT212>
2017-03-07 15:50:20 +0530

    last commit

--- src/com/example/notification/MainActivity.java
+++ src/com/example/notification/MainActivity.java
@@ -3,46 +3,44 @@ package com.example.notification;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+public class MainActivity extends ActionBarActivity {
+	Button b1;
 
-public class MainActivity extends Activity {
-	   Button b1;
-	   protected void onCreate(Bundle savedInstanceState) {
-	      super.onCreate(savedInstanceState);
-	      setContentView(R.layout.activity_main);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		b1 = (Button) findViewById(R.id.b1);
 
-	      b1 = (Button)findViewById(R.id.b1);
-	      b1.setOnClickListener(new View.OnClickListener() {
-	         @Override
-	         public void onClick(View v) {
-	            addNotification();
-	         }
-	      });
-	   }
+		b1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				PendingIntent pIntent = PendingIntent.getActivity(
+						MainActivity.this, 0, intent, 0);
 
-	   private void addNotification() {
-	      NotificationCompat.Builder builder =
-	         new NotificationCompat.Builder(this)
-	         //.setSmallIcon(R.drawable.abc)
-	         .setContentTitle("Notifications Example")
-	         .setContentText("This is a test notification");
+				Notification notify = new Notification.Builder(
+						MainActivity.this).setTicker("TicketerTitle")
+						.setContentTitle("Content Title")
+						.setContentText(" hello Everyone it's notification prgrm??")
+						.setSmallIcon(R.drawable.ic_launcher)
+						.addAction(R.drawable.ic_launcher, "Action 1", pIntent)
+						.setContentIntent(pIntent).getNotification();
 
-	      Intent notificationIntent = new Intent(this, MainActivity.class);
-	      PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
-	         PendingIntent.FLAG_UPDATE_CURRENT);
-	      builder.setContentIntent(contentIntent);
-
-	      // Add as notification
-	      NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-	      manager.notify(0, builder.build());
-	   }
+				notify.flags = Notification.FLAG_AUTO_CANCEL;
+				NotificationManager notif = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+				notif.notify(0, notify);
+			}
+		});
 	}
+}
--- src/com/example/notification/NotificationView.java
+++ src/com/example/notification/NotificationView.java
@@ -1,13 +1,34 @@
 package com.example.notification;
 
-
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
-public class NotificationView extends Activity{
-   @Override
-   public void onCreate(Bundle savedInstanceState){
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.second_activity);
-   }
-}
+import android.widget.TextView;
 
+public class NotificationView extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second_activity);
+		CharSequence s = "Inside the activity of Notification one ";
+		int id = 0;
+		Bundle extras = getIntent().getExtras();
+		if (extras == null) {
+			s = "error";
+		}
+		else {
+			id = extras.getInt("notificationId");
+		}
+		TextView t = (TextView) findViewById(R.id.text1);
+		s = s + "with id = " + id;
+		t.setText(s);
+		NotificationManager myNotificationManager =
+		(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		myNotificationManager.cancel(id);
+
+	}
+
+}
