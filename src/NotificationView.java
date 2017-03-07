fa00ca6d9f85bc77c3e7ea68f23de1ce7f4a5567
ITLAB <ITLAB@IT212>
2017-03-07 13:31:54 +0530

    commit1

---
+++ src/NotificationView.java
@@ -0,0 +1,13 @@
+package com.example.notification;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.appcompat.R;
+
+public class NotificationView extends Activity{
+   @Override
+   public void onCreate(Bundle savedInstanceState){
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.second_activity);
+   }
+}
